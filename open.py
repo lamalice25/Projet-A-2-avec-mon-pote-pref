58fca82be65c4fdd42d3b06123fe5b39976c9116
lamalice25 <titijiyan974@gmail.com>
2024-04-10 20:46:03 +0400

    ok

--- open.py
+++ open.py
@@ -22,7 +22,7 @@ t_sur = input("voulais vous suprimer les donnée de votre disque dur ? : ")
 if 'oui' in t_sur:
     droid.say('Ok, nous fermons lapp')
     droid.runAndWait()
-    exit()
+    
 elif 'non' in t_sur:
     droid.say('Importations de la mère a Fayze tiré du bas y.t.b')
     droid.runAndWait()
@@ -43,7 +43,7 @@ elif 'non' in t_sur:
     messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
     messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
     messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
-    
+        
     time.sleep(5)
     
     messagebox.showinfo('Hacking Finish', 'Nous avons fini lenvoie sur le web')
