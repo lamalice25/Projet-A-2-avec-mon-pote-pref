327d31d731b3f72f68a7610568de9153be79b210
lamalice25 <titijiyan974@gmail.com>
2024-04-10 20:39:31 +0400

    ok

---
+++ open.py
@@ -0,0 +1,51 @@
+import pyttsx3
+import time
+import messagebox
+
+
+droid = pyttsx3.init()
+
+droid.say('Bonjour nous somme en train de hacké votre ordinateur')
+droid.runAndWait()
+
+
+
+time.sleep(5)
+
+droid.say('Voulais vous suprimer les donnée de votre disque dur ?')
+droid.runAndWait()
+
+
+t_sur = input("voulais vous suprimer les donnée de votre disque dur ? : ")
+
+
+if 'oui' in t_sur:
+    droid.say('Ok, nous fermons lapp')
+    droid.runAndWait()
+    exit()
+elif 'non' in t_sur:
+    droid.say('Importations de la mère a Fayze tiré du bas y.t.b')
+    droid.runAndWait()
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    messagebox.showerror('Hacking computer', 'Nous somme en train denvoyer les donnée sur le web')
+    
+    time.sleep(5)
+    
+    messagebox.showinfo('Hacking Finish', 'Nous avons fini lenvoie sur le web')
+
+
